df30ec7aee75cb8b6f06b6f5ee8d529cd9a8d936
ssadgyan <dsvvsadgyansinghindo@gmail.com>
2025-09-29 09:46:07 +0530

    Removed config.py from tracking and updated .gitignore

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-MONGO_URI = "mongodb+srv://sadgyan:sadgyan12345678@cluster0.7nexhms.mongodb.net/fraud_detection?retryWrites=true&w=majority&appName=Cluster0"
-
-JWT_SECRET = "12343dsfasd123"
-JWT_ALGO = "HS256"
