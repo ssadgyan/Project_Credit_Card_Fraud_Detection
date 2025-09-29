a73bef4528238ff526f4b5e395988e0c2673fb0b
ssadgyan <dsvvsadgyansinghindo@gmail.com>
2025-09-30 04:16:56 +0530

    push config.py

---
+++ backend/config.py
@@ -0,0 +1,4 @@
+MONGO_URI = "mongodb+srv://sadgyan:sadgyan12345678@cluster0.7nexhms.mongodb.net/fraud_detection?retryWrites=true&w=majority"
+JWT_SECRET = "12343dsfasd123"
+JWT_ALGO = "HS256"
+GOOGLE_CLIENT_ID = "105149798415-0b8s1bet2k0c2uce5ga4rifdmp04s572.apps.googleusercontent.com"
